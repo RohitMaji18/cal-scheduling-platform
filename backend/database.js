ca6da9d0580a4812ffd757dce7059d9c692b7907
Rohit Maji <rohitmaji1817@gmail.com>
2026-01-08 17:46:02 +0530

    Ready for Vercel deployment

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,15 @@
+// database.js (Backend folder mein)
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
+  // Ye line sabse important hai.
+  // Deploy hone par ye 'DATABASE_URL' variable dhoondega.
+  // Laptop par ye tumhare .env file se values lega.
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false, // Cloud DB ke liye zaroori hai
+  },
 });
 
 module.exports = pool;
